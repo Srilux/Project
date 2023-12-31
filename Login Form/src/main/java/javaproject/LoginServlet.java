4deb7f71cbaa150de2e8a534a35c9812751ecb1e
Srilux <apsanthanam1999@gmail.com>
2023-12-31 15:36:47 +0530

    Login

---
+++ Login Form/src/main/java/javaproject/LoginServlet.java
@@ -0,0 +1,76 @@
+package javaproject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	 public LoginServlet() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
+
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String uname=request.getParameter("uname");
+		String password=request.getParameter("password");
+					
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Form","root","root");
+			Statement st=conn.createStatement();
+			String query ="select * from user where uname='"+uname+"' and password='"+password+"'";
+			ResultSet rs=st.executeQuery(query);
+		if(rs.next()) {
+//			RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+//			rd.forward(request, response);
+			out.print("<h1>Login Successfully!</h1>");
+		}else {
+			out.print("<h1>Login Failed!!</h1>");
+		}
+		rs.close();
+		st.close();
+		conn.close();
+		} catch (ClassNotFoundException e) {
+			out.print("<h1>Something Wrong!</h1>");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			out.print("<h1>Something went Wrong!</h1>");
+			e.printStackTrace();
+	    }
+			
+		
+		}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			doGet(request, response);
+		}
+
+	}
